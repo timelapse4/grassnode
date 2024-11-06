e2c97d728fb51d12d08990d536b48ff38e6826a1
timelapse4 <redhats.cc@gmail.com>
2024-11-07 02:33:08 +0700

    Update grassdesktop.py

--- grassdesktop.py
+++ grassdesktop.py
@@ -10,10 +10,9 @@ from loguru import logger
 from websockets_proxy import Proxy, proxy_connect
 from fake_useragent import UserAgent
 
-user_agent = UserAgent(os='windows', platforms='pc', browsers='chrome')
-random_user_agent = user_agent.random
-
 async def connect_to_wss(socks5_proxy, user_id):
+    user_agent = UserAgent(os='windows', platforms='pc', browsers='chrome')
+    random_user_agent = user_agent.random
     device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
     logger.info(device_id)
     while True:
